7ff206b39e195eb8d6912d6d58e2ebd9e8524ff5
Srikrushna <srikrushna.sethi54@gmail.com>
2022-11-03 15:59:01 +0530

    troubleshoot

--- simple.java
+++ simple.java
@@ -2,5 +2,8 @@ class Simple{
         public static void main(String args[]){  
          System.out.println("Hello naveen java");  
   System.out.println("Hello Krushna java"); 
+ System.out.println("Hello Krushna java"); 
+ System.out.println("Hello Krushna java"); 
+ System.out.println("Hello Krushna java"); 
         }  
     }  
